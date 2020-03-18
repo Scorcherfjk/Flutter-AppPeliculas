1e20381ac1b4c672767673e61285c85aefd16825
Fjavier De Freitas <32197704+Scorcherfjk@users.noreply.github.com>
2020-03-18 18:29:14 -0500

    peliculas populares en el final del home

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,16 +1,19 @@
 import 'package:curso_peliculas/src/providers/peliculas_provider.dart';
+import 'package:curso_peliculas/src/widgets/movie_horizontal.dart';
 import 'package:flutter/material.dart';
 
 import 'package:curso_peliculas/src/widgets/card_swiper_widget.dart';
 
 class HomePage extends StatelessWidget {
-
   HomePage({Key key}) : super(key: key);
 
   final peliculasProvider = new PeliculasProvider();
 
   @override
   Widget build(BuildContext context) {
+
+    peliculasProvider.getPopular();
+
     return Container(
       child: Scaffold(
         appBar: AppBar(
@@ -18,26 +21,20 @@ class HomePage extends StatelessWidget {
           title: Text('Peliculas'),
           backgroundColor: Colors.indigoAccent,
           actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.search),
-              onPressed: (){}
-            ),
+            IconButton(icon: Icon(Icons.search), onPressed: () {}),
           ],
         ),
         body: Container(
           child: Column(
-            children: <Widget>[
-              _swiperCard(),
-            ],
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[_swiperCard(), _footer(context)],
           ),
         ),
-
       ),
     );
   }
 
   Widget _swiperCard() {
-
     return FutureBuilder(
       future: peliculasProvider.getEnCines(),
       builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
@@ -45,11 +42,7 @@ class HomePage extends StatelessWidget {
           return CardSwiper(elementos: snapshot.data);
         } else {
           return Container(
-            height: 400.0,
-            child: Center(
-              child: CircularProgressIndicator()
-            )
-          );
+              height: 400.0, child: Center(child: CircularProgressIndicator()));
         }
       },
     );
@@ -57,6 +50,33 @@ class HomePage extends StatelessWidget {
     // return CardSwiper(elementos: [10,11,12,13,14,15,16,17]);
   }
 
-
-  
+  Widget _footer(context) {
+    return Container(
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(left: 20.0),
+            child: Text(
+              'Populares',
+              style: Theme.of(context).textTheme.subhead,
+            ),
+          ),
+          StreamBuilder(
+            stream: peliculasProvider.popularesStream,
+            builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+              if (snapshot.hasData) {
+                return MovieHorizontal(peliculas: snapshot.data, siguientePagina: peliculasProvider.getPopular,);
+              } else {
+                return Container(
+                    height: 200.0,
+                    child: Center(child: CircularProgressIndicator()));
+              }
+            },
+          ),
+        ],
+      ),
+    );
+  }
 }
--- lib/src/providers/peliculas_provider.dart
+++ lib/src/providers/peliculas_provider.dart
@@ -1,31 +1,62 @@
+import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:curso_peliculas/src/models/pelicula_model.dart';
 
 class PeliculasProvider {
-
-  String _apiKey     = 'b7cccf6248b00c03d14abb919c3fa17e';
-  String _url        = 'api.themoviedb.org';
-  String _language   = 'es-ES';
+  String _apiKey = 'b7cccf6248b00c03d14abb919c3fa17e';
+  String _url = 'api.themoviedb.org';
+  String _language = 'es-ES';
   String _apiVersion = '/3';
+  int _popularesPage = 0;
+  bool _cargando = false;
 
+  List<Pelicula> _populares = new List();
 
-  Future<List<Pelicula>>getEnCines() async{
+  final _popularesStreamController =
+      StreamController<List<Pelicula>>.broadcast();
 
-    final url = Uri.https(_url, '$_apiVersion/movie/now_playing',{
-      'api_key':_apiKey,
-      'language':_language
-    });
+  Function(List<Pelicula>) get popularesSink =>
+      _popularesStreamController.sink.add;
 
+  Stream<List<Pelicula>> get popularesStream =>
+      _popularesStreamController.stream;
+
+  void disposeStreams() {
+    _popularesStreamController.close();
+  }
+
+  Future<List<Pelicula>> _procesarRespuesta(url) async {
     final resp = await http.get(url);
     final decodedData = json.decode(resp.body);
 
     final peliculas = Peliculas.fromJsonList(decodedData['results']);
 
     return peliculas.items;
-
   }
 
+  Future<List<Pelicula>> getEnCines() async {
+    final url = Uri.https(_url, '$_apiVersion/movie/now_playing',
+        {'api_key': _apiKey, 'language': _language});
 
-  
+    return await _procesarRespuesta(url);
+  }
+
+  Future<List<Pelicula>> getPopular() async {
+    if (_cargando) return [];
+
+    _cargando = true;
+    _popularesPage++;
+
+    final url = Uri.https(_url, '$_apiVersion/movie/popular',
+        {'api_key': _apiKey, 'language': _language, 'page': '$_popularesPage'});
+
+    final resp = await _procesarRespuesta(url);
+
+    _populares.addAll(resp);
+    popularesSink(_populares);
+
+    _cargando = false;
+    return resp;
+  }
 }
